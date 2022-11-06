c01abfd5e9c1f78051ed39156b1d591e3452e39e
ashyan <ashyan99@gmail.com>
2022-11-06 01:22:45 -0700

    -Added Discord bot support. Bot will send message to server on case of ban/death and unban

---
+++ Spigot Ban Plugin/src/main/java/ashyan/DiscordBot.java
@@ -0,0 +1,34 @@
+package ashyan;
+
+
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.Permission;
+import net.dv8tion.jda.api.entities.Member;
+import net.dv8tion.jda.api.entities.User;
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import net.dv8tion.jda.api.events.interaction.component.ButtonInteractionEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.dv8tion.jda.api.interactions.InteractionHook;
+import net.dv8tion.jda.api.interactions.commands.DefaultMemberPermissions;
+import net.dv8tion.jda.api.interactions.commands.OptionMapping;
+import net.dv8tion.jda.api.interactions.commands.build.Commands;
+import net.dv8tion.jda.api.interactions.commands.build.OptionData;
+import net.dv8tion.jda.api.interactions.components.buttons.Button;
+import net.dv8tion.jda.api.requests.GatewayIntent;
+import net.dv8tion.jda.api.requests.restaction.CommandListUpdateAction;
+
+import java.util.EnumSet;
+import java.util.concurrent.TimeUnit;
+
+import static net.dv8tion.jda.api.interactions.commands.OptionType.*;
+
+public class DiscordBot extends ListenerAdapter {
+    public DiscordBot() {
+
+    }
+
+    public void logDeath(String name) {
+
+    }
+}
